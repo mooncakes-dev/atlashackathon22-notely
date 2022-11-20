5f6b2c56add73f01914541a7ba06c5a3eb8fbcaf
mooncakes-dev <mneshenko@gmail.com>
2022-11-20 16:23:14 -0500

    updated ignore, added nav bar and vue project

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="container">
+  <h1>Notely</h1>
+  <el-autocomplete
+      placeholder="Search"
+      clearable
+      class="w-50 m-2"
+      :prefix-icon="Search"
+      size="large"
+  />
+</div>
+</template>
+
+<script>
+import { Search } from '@element-plus/icons-vue'
+
+export default {
+  name: "Navbar",
+  components: {
+    Search
+  }
+}
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Navbar from '../components/Navbar.vue';
+</script>
+
+<template>
+    <el-container>
+        <el-header>
+            <Navbar />
+        </el-header>
+    </el-container>
+    <main></main>
+</template>
+
+<style scoped lang="scss">
+.el-container {
+    margin: 0 30em;
+}
+</style>
