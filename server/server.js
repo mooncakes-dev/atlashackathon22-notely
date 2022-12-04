edda4cbeeefa12a103d8335ac4d28b99dbba3cfe
mooncakes-dev <mneshenko@gmail.com>
2022-12-04 14:45:38 -0500

    simple autocomplete and search

--- server/server.js
+++ server/server.js
@@ -11,6 +11,7 @@ const app = express();
 // middleware
 app.use(express.json());
 
+const client = new MongoClient(process.env.ATLAS_URI);
 // cors
 app.use(
   cors({
@@ -25,6 +26,30 @@ app.use((req, res, next) => {
 
 // routes
 app.use("/api/work-notes", workNotesRoutes);
+app.get("/search", async (request, response) => {
+  try {
+    let result = await client
+      .db("test")
+      .collection("worknotes")
+      .aggregate([
+        {
+          $search: {
+            autocomplete: {
+              query: `${request.query.term}`,
+              path: "tags",
+              fuzzy: {
+                maxEdits: 2,
+              },
+            },
+          },
+        },
+      ])
+      .toArray();
+    response.send(result);
+  } catch (e) {
+    response.status(500).send({ message: e.message });
+  }
+});
 
 // connect to db
 mongoose
